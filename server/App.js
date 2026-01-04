56549c495426a22fe7b34966ede7fb639266e6e8
VoidOfMaya <mayaofthevoid@gmail.com>
2026-01-04 15:06:30 +0200

    docs (server/App.js): documents common shell node commands

---
+++ server/App.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/*
+common node bash commands:-
+
+Pass string as argument to node
+=> node -e "console.log(123)"
+
+Restart the application automatically
+=> node --watch app.js
+
+Run a task with Node.js
+=> node --run {package.json command}/
+=> node --run test/
+=> node --run dev
+
+*/
+// your javascript code
