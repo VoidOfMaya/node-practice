62870b0e6a74c765a159f0431c3f740a2d57e9c8
VoidOfMaya <mayaofthevoid@gmail.com>
2026-01-06 12:34:16 +0200

    ejs and data transfer to ejs elements/pages

--- server/express.js
+++ server/express.js
@@ -9,6 +9,11 @@ import path from 'path';
 //setting up server with express.js
 const app = express();
 
+//register view engine
+
+app.set('view engine','ejs');
+
+
 app.listen(3000)
 
 
@@ -17,21 +22,35 @@ app.listen(3000)
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.get('/',(req, res)=>{
+    //html
     //interprets content type based on sent content
-    res.sendFile('./htmlpages/main.html', {root: __dirname});
+    //==>res.sendFile('./htmlpages/main.html', {root: __dirname});
+
+    //ejs
+    res.render('main',{title: 'Home'});
 
 });
 app.get('/about',(req, res)=>{
+    //html
     //expects absolute path, if relative  it requires root directory
     //to be specified
-    res.sendFile('./htmlpages/about.html', {root: __dirname});
+    //==>res.sendFile('./htmlpages/about.html', {root: __dirname});
+
+    //ejs
+    res.render('about');
     
 });
 app.get('/contact',(req, res)=>{
-    
-    res.sendFile('./htmlpages/contact.html', {root: __dirname});
-    
+    //html
+    //==>res.sendFile('./htmlpages/contact.html', {root: __dirname});
+
+    //ejs
+    res.render('contact');
 });
+
+app.get('/create',(req, res)=>{
+    res.render('create')
+})
 
 //=== 300 redirects ===
 app.get('/about-us',(req,res)=>{
@@ -41,6 +60,6 @@ app.get('/about-us',(req,res)=>{
 //note : make sure this is always the last method in a router, this acts as the default in a switch case
 app.use((re, res)=>{
     res.status(404)
-       .sendFile('./htmlpages/404.html', {root: __dirname})
+       .render('404')
     
 })
